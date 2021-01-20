d18e0e265755e22d2b0b2d8f75a5c2fb95098433
Júlia <juhbotelho.contato@gmail.com>
2021-01-20 16:09:34 -0300

    feat: Adding votes and sending to API

--- src/pages/ViewPoll.js
+++ src/pages/ViewPoll.js
@@ -3,43 +3,62 @@ import React, { useEffect, useState } from 'react'
 import Button from '../components/Button'
 
 export default function ViewPoll({ match }) {
-    const [poll, setPoll] = useState(null)
+  const [poll, setPoll] = useState(null)
+  const [voted, setVoted] = useState(false)
 
-    const fetchPoll = async () => {
-        const response = await fetch(`http://localhost:5000/polls/${match.params.poll}`)
+  const fetchPoll = async () => {
+    const response = await fetch(`http://localhost:5000/polls/${match.params.poll}`)
 
-        const data = await response.json()
+    const data = await response.json()
 
-        setPoll(data)
-    }
+    setPoll(data)
+  }
 
-    useEffect(() => {
-        fetchPoll()
-    }, [])
+  useEffect(() => {
+    fetchPoll()
+  }, [])
 
-    return (
-        <div className="container mx-auto mt-16 px-5">
-            <h1 className="my-5 text-3xl text-center">
-                Vote no seu restaurante favorito
-            </h1>
+  const vote = async (title, restaurants, restaurant) => {
+    await fetch(`http://localhost:5000/polls/${match.params.poll}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title,
+        restaurants,
+        restaurant
+      })
+    })
 
-            {poll ? (
-                <div className="w-full max-w-3xl mx-auto bg-white shadow">
-                    <header className='px-5 py-4 flex justify-between items-center'>
-                        {poll.title}
-                    </header>
+    setVoted(true)
+  }
 
-                    {poll.restaurants.map(restaurant => {
-                        return (
-                            <div className='px-5 py-4 border-t border-gray-400 flex justify-between items-center' key={restaurant._id}>
-                                {restaurant.name}
+  return (
+    <div className="container mx-auto mt-16 px-5">
+      <h1 className="my-5 text-3xl text-center">
+        Vote no seu restaurante favorito
+      </h1>
 
-                                <Button>Votar</Button>
-                            </div>
-                        )
-                    })}
-                </div>
-            ) : null}
+      {poll ? (
+        <div className="w-full max-w-3xl mx-auto bg-white shadow">
+          <header className='px-5 py-4 flex justify-between items-center'>
+            {poll.title}
+          </header>
+
+          {poll.restaurants.map(restaurant => {
+            return (
+              <div className='px-5 py-4 border-t border-gray-400 flex justify-between items-center' key={restaurant._id}>
+                {restaurant.name}
+
+                {!voted ? (
+                  <Button onClick={() => vote(poll.title, poll.restaurants.map(({name}) => name) , restaurant._id)}>Vote</Button>
+                ): <span className='text-blue-500'>29</span>}
+              </div>
+            )
+          })}
         </div>
-    )
+      ) : null}
+    </div>
+  )
 }
